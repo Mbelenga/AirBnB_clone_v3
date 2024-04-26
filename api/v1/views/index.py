55a6b58641b22401a2816df061e8eb44b53faeba
Mbelenga <mbelengaterence@gmail.com>
2024-04-26 17:23:30 +0300

    updated

---
+++ api/v1/views/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script returns a JSON response """
+
+from flask import jsonify
+from api.v1.views import app_views
+from models import storage
+
+@app_views.route('/status')
+def status_check():
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def object_stats():
+    """ Retrieves number of objects """
+    objects = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User'),
+    return jsonify(objects)
