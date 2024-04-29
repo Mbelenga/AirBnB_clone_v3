d9279f58055dd0fdfdd492ccaffc6655e400b3b6
Mbelenga <Mbelengaterence@gmail.com>
2024-04-29 11:02:22 -0700

    fixing errors

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-""" This script returns a JSON response """
+"""Returns a Json response"""
 
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 @app_views.route('/status')
 def status_check():
+    '''Returns status code'''
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def object_stats():
-    """ Retrieves number of objects """
+    """Retrieves the no of each object by type"""
     objects = {
             "amenities": storage.count('Amenity'),
             "cities": storage.count('City'),
@@ -19,4 +22,5 @@ def object_stats():
             "reviews": storage.count('Review'),
             "states": storage.count('State'),
             "users": storage.count('User'),
+            }
     return jsonify(objects)
